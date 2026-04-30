7570437902fccfcf88abd97b4e7db0e15f7515b2
ragepanz <ivnedwrd09@gmail.com>
2026-04-30 09:24:30 +0700

    update: delete button back to dashboard + reset db ke awal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,11 +45,7 @@
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"/><line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="18" x2="21" y2="18"/></svg>
                 </button>
 
-                @if(!request()->routeIs('dashboard'))
-                    <a href="{{ route('dashboard') }}" class="btn-back" title="Back to Dashboard">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg>
-                    </a>
-                @endif
+
 
                 <a href="{{ route('dashboard') }}" class="navbar-brand">
                     <div class="navbar-logo-wrap">
